7c3a30dbbbb1d21fb237cb173d891c98bc25f785
Jumper <dcohen@pearsonpkg.com>
2019-11-06 19:26:37 -0500

    One of the motors is staying in brake mode. We tried to fix it but it stopped working again.

--- src/main/java/frc/robot/Climber.java
+++ src/main/java/frc/robot/Climber.java
@@ -1,50 +1,50 @@
 package frc.robot;
-import edu.wpi.first.wpilibj.DigitalInput;
+//import edu.wpi.first.wpilibj.DigitalInput;
 
 // Spark Max libraries
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMax.IdleMode;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+//import com.revrobotics.CANSparkMax;
+//import com.revrobotics.CANSparkMax.IdleMode;
+//import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Climber {
-	private CANSparkMax topClimberMotor;
-	private CANSparkMax bottomClimberMotor;
+//	private CANSparkMax topClimberMotor;
+//	private CANSparkMax bottomClimberMotor;
 	
 	public Climber() {
-		topClimberMotor = new CANSparkMax(TalonPort.topClimber, MotorType.kBrushless);
-		bottomClimberMotor = new CANSparkMax(TalonPort.bottomClimber, MotorType.kBrushless);
+//		topClimberMotor = new CANSparkMax(TalonPort.topClimber, MotorType.kBrushless);
+//		bottomClimberMotor = new CANSparkMax(TalonPort.bottomClimber, MotorType.kBrushless);
 
 		// Initialize the Sparks before anything else
-		topClimberMotor.restoreFactoryDefaults();
-		bottomClimberMotor.restoreFactoryDefaults();
+//		topClimberMotor.restoreFactoryDefaults();
+//		bottomClimberMotor.restoreFactoryDefaults();
 
 		// Set the brake mode of the climber motors
-		topClimberMotor.setIdleMode(IdleMode.kBrake);
-		bottomClimberMotor.setIdleMode(IdleMode.kBrake);
+//		topClimberMotor.setIdleMode(IdleMode.kBrake);
+//		bottomClimberMotor.setIdleMode(IdleMode.kBrake);
 	}
 
 	// This is called during the speed ramp-up of the climber
 	public void variableClimb(double speed) {
 		speed = (speed > 1.0) ? 1.0 : speed;	// set speed to 1.0 if we go over 100%
-		topClimberMotor.set(-speed);
-		bottomClimberMotor.set(-speed);
+//		topClimberMotor.set(-speed);
+//		bottomClimberMotor.set(-speed);
 	}
 
 	// This is called after the speed ramp-up of the climber
 	public void windClimb() {
-		topClimberMotor.set(-1.0);
-		bottomClimberMotor.set(-1.0);
+//		topClimberMotor.set(-1.0);
+//		bottomClimberMotor.set(-1.0);
 	}
 
 	public void unwindClimb() {
-		topClimberMotor.set(0.5);
-		bottomClimberMotor.set(0.5);
+//		topClimberMotor.set(0.5);
+//		bottomClimberMotor.set(0.5);
 	}
 
 	public void stop()
 	{
-		topClimberMotor.set(0);
-		bottomClimberMotor.set(0);
+//		topClimberMotor.set(0);
+//		bottomClimberMotor.set(0);
 	}
 	
 }
--- src/main/java/frc/robot/TalonPort.java
+++ src/main/java/frc/robot/TalonPort.java
@@ -5,8 +5,8 @@ public class TalonPort
 	static int intakeTop = 10;			// talon #8, invert = false; breaker #2
 	static int intakeBottom = 11;		// talon #9, invert = default; breaker #4
 	static int wristMotor = 17;			// spark #17, motor 7; breaker #14
-	static int topClimber = 15;			// talon #15; breaker #0
-	static int bottomClimber = 16;		// talon #16; breaker #1
+//	static int topClimber = 15;			// talon #15; breaker #0
+//	static int bottomClimber = 16;		// talon #16; breaker #1
 	static int frontRightMotor = 7;
 	//static int centerRightMotor = 9;	// must be inversed
 	static int rearRightMotor = 2;		// talon #3, invert = false; tal2; breaker #13
